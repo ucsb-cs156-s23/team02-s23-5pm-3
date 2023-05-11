2d9bd31b7ae1d3d46ba06ad48f6d39c9570b01fa
Anthonypella <anthonypella0@gmail.com>
2023-05-10 17:48:17 -0700

    add controllers

--- src/main/java/edu/ucsb/cs156/example/entities/Park.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Park.java
@@ -1,7 +1,5 @@
 package edu.ucsb.cs156.example.entities;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,7 +17,7 @@ import lombok.Builder;
 @Entity(name = "park")
 public class Park {
   @Id
-  @GeneratedValue(strategy = GenerationType.Iterable)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
   private String name;
